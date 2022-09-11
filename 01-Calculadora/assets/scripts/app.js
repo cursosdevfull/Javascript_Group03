bd4b9045a38ac924441045a0d4b67ea8cf794e6c
Sergio Hidalgo <shidalgoc@auna.pe>
2022-09-11 17:59:59 -0500

    dividiendo en funciones

--- 01-Calculadora/assets/scripts/app.js
+++ 01-Calculadora/assets/scripts/app.js
@@ -12,4 +12,28 @@ function add() {
   outputResult(currentResult, calcDescription);
 }
 
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} - ${enteredNumber}`;
+  currentResult = currentResult - enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} * ${enteredNumber}`;
+  currentResult = currentResult * enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} / ${enteredNumber}`;
+  currentResult = currentResult / enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
