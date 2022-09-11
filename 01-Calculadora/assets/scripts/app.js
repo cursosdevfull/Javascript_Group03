0e03a5f3568fb1fcb66bf10f106c29972c96938e
Sergio Hidalgo <shidalgoc@auna.pe>
2022-09-11 17:35:27 -0500

    ambito

--- 01-Calculadora/assets/scripts/app.js
+++ 01-Calculadora/assets/scripts/app.js
@@ -1,23 +1,13 @@
 const defaultResult = 0;
 let currentResult = 0;
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  //const inputEntered = userInput.value;
+  //currentResult = currentResult + parseInt(userInput.value);
+  currentResult = currentResult + +userInput.value;
+  //console.log('add');
+  //console.log(currentResult);
+  outputResult(currentResult, '');
 }
 
-/* const additionalResult = add(20, 30)
-currentResult = additionalResult */
-
-currentResult = add(20, 30);
-
-let calculationDescription = `(
-    (${defaultResult} + 10) * 3)
-    / 2
-    - 1
-`;
-
-//let errorMessage = "An error's occurred!";
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
