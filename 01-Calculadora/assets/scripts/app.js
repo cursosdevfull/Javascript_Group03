6995b35ab1f5aa263a85fca2a9524f28e7777fd5
Sergio Hidalgo <shidalgoc@auna.pe>
2022-09-11 17:04:59 -0500

    usando constantes

--- 01-Calculadora/assets/scripts/app.js
+++ 01-Calculadora/assets/scripts/app.js
@@ -1,7 +1,17 @@
+const defaultResult = 0;
 let currentResult = 0;
 
 //currentResult = 20;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-outputResult(currentResult, "prueba");
+// let calculationDescription = '((' + defaultResult + ' + 10) * 3) / 2 - 1';
+let calculationDescription = `(
+    (${defaultResult} + 10) * 3)
+    / 2
+    - 1
+`;
+
+let errorMessage = "An error's occurred!";
+
+outputResult(currentResult, errorMessage);
