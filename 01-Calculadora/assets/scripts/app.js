fcade5a469cb0407336e24d8bc4d5e90d6404ec4
Sergio Hidalgo <shidalgoc@auna.pe>
2022-09-11 17:47:12 -0500

    dividiendo en funciones

--- 01-Calculadora/assets/scripts/app.js
+++ 01-Calculadora/assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = 0;
 
+function getUserNumberInput() {
+  return +userInput.value;
+}
+
 function add() {
-  //const inputEntered = userInput.value;
-  //currentResult = currentResult + parseInt(userInput.value);
-  currentResult = currentResult + +userInput.value;
-  //console.log('add');
-  //console.log(currentResult);
-  outputResult(currentResult, '');
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
