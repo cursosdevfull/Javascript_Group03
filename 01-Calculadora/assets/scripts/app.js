ab16693f8fd54cd6114af3db3abb0fa944e1ebef
Sergio Hidalgo <shidalgoc@auna.pe>
2022-09-11 17:17:43 -0500

    retornando valor

--- 01-Calculadora/assets/scripts/app.js
+++ 01-Calculadora/assets/scripts/app.js
@@ -1,17 +1,23 @@
 const defaultResult = 0;
 let currentResult = 0;
-
-//currentResult = 20;
-
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-// let calculationDescription = '((' + defaultResult + ' + 10) * 3) / 2 - 1';
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+/* const additionalResult = add(20, 30)
+currentResult = additionalResult */
+
+currentResult = add(20, 30);
+
 let calculationDescription = `(
     (${defaultResult} + 10) * 3)
     / 2
     - 1
 `;
 
-let errorMessage = "An error's occurred!";
+//let errorMessage = "An error's occurred!";
 
-outputResult(currentResult, errorMessage);
+outputResult(currentResult, calculationDescription);
