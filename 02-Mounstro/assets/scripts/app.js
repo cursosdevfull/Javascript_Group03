30ca003f304eef36eb31f655b5b6ac43a2849d62
Sergio Hidalgo <shidalgoc@auna.pe>
2022-09-18 17:44:39 -0500

    health extra

--- 02-Mounstro/assets/scripts/app.js
+++ 02-Mounstro/assets/scripts/app.js
@@ -1,11 +1,35 @@
 const ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 15;
+const HEALTH_VALUE = 20;
 
 const choseMaxLife = 100;
 let currentMonsterHealth = choseMaxLife;
 let currentPlayerHealth = choseMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(choseMaxLife);
+
+function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(currentPlayerHealth);
+  }
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('Player win');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('Monster win');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('Draw');
+  }
+}
 
 function attackMonsterHandler(mode) {
   let maxDamage;
@@ -17,10 +41,8 @@ function attackMonsterHandler(mode) {
 
   const mounstroDamage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= mounstroDamage;
-  const playerDamage = dealPlayerDamage(maxDamage);
-  currentPlayerHealth -= playerDamage;
 
-  alert('Attack detected');
+  endRound();
 }
 
 function attackHandler() {
@@ -31,5 +53,27 @@ function strongAttackHandler() {
   attackMonsterHandler('STRONG_ATTACK');
 }
 
+function healPlayerHandler() {
+  if (currentPlayerHealth <= 0 || currentMonsterHealth <= 0) {
+    return;
+  }
+
+  hasBonusLife = false;
+  removeBonusLife();
+
+  let healthValue;
+  if (currentPlayerHealth >= choseMaxLife - HEALTH_VALUE) {
+    alert('Player cannot more life than health maximum');
+    healthValue = choseMaxLife - currentPlayerHealth;
+  } else {
+    healthValue = HEALTH_VALUE;
+  }
+
+  currentPlayerHealth += healthValue;
+  increasePlayerHealth(healthValue);
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
