93d9799930b93de58ea39686078a388993fbf295
Sergio Hidalgo <shidalgoc@auna.pe>
2022-09-25 17:23:01 -0500

    juego piedra, papel y tijera

---
+++ Conocimientos/05-use-strict/app.js
@@ -0,0 +1,16 @@
+'use strict';
+const age = 30;
+
+console.log(age);
+
+const info = { username: 'user1' };
+console.log(info);
+
+function printInfoUser() {
+  const username = 'Carlos';
+  const ageUser = 14;
+
+  console.log(username, ageUser);
+}
+
+printInfoUser();
